545be1fd2eeabddea3944973955c1ba3502c6a92
oyangferreira <yang.ferreira2023@gmail.com>
2024-07-04 10:03:05 -0300

    bank-operation

--- primeiro-projeto-java/src/App.java
+++ primeiro-projeto-java/src/App.java
@@ -1,5 +1,48 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Teste");
+        Scanner scanner = new Scanner(System.in);
+        double saldo = 0;
+        boolean continuar = true;
+
+        while (continuar) {
+            System.out.println("Escolha uma opção:");
+            System.out.println("1: Depositar");
+            System.out.println("2: Sacar");
+            System.out.println("3: Consultar Saldo");
+            System.out.println("0: Encerrar");
+            
+            int opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    System.out.print("Digite o valor a ser depositado: ");
+                    double deposito = scanner.nextDouble();
+                    saldo += deposito;
+                    System.out.printf("Saldo atual: %.1f\n", saldo);
+                    break;
+                case 2:
+                    System.out.print("Digite o valor a ser sacado: ");
+                    double saque = scanner.nextDouble();
+                    if (saque <= saldo) {
+                        saldo -= saque;
+                        System.out.printf("Saldo atual: %.1f\n", saldo);
+                    } else {
+                        System.out.println("Saldo insuficiente.");
+                    }
+                    break;
+                case 3:
+                    System.out.printf("Saldo atual: %.1f\n", saldo);
+                    break;
+                case 0:
+                    System.out.println("Programa encerrado.");
+                    continuar = false;
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+        }
+        scanner.close();
     }
 }
